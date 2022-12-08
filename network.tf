2453ec670f00a1be37512b28ab2fe0456f3e103a
nwjbrandon <nwjbrandon@gmail.com>
2022-12-08 14:39:34 +0800

    comment config

--- network.tf
+++ network.tf
@@ -1,12 +1,12 @@
-resource "aws_internet_gateway" "prod-igw" {
-    vpc_id = "${aws_vpc.prod-vpc.id}"
-    tags = {
-        Name = "prod-igw"
-    }
-}
+# resource "aws_internet_gateway" "prod-igw" {
+#     vpc_id = "${aws_vpc.prod-vpc.id}"
+#     tags = {
+#         Name = "prod-igw"
+#     }
+# }
 
-resource "aws_route" "public-route" {
-  route_table_id         = aws_vpc.prod-vpc.main_route_table_id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.prod-igw.id
-}
+# resource "aws_route" "public-route" {
+#   route_table_id         = aws_vpc.prod-vpc.main_route_table_id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.prod-igw.id
+# }

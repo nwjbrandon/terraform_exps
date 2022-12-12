89f4ff0995f77a5f3fdc08a2f9fbfac4fd9d6ff9
nwjbrandon <nwjbrandon@gmail.com>
2022-12-12 15:23:22 +0800

    add security groups

--- security_groups.tf
+++ security_groups.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "ssh" {
     name        = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-ssh"
-    description = "Allow TLS inbound traffic"
+    description = "Allow ssh inbound traffic"
     vpc_id      = aws_vpc.vpc.id
 
     ingress {
@@ -10,6 +10,24 @@ resource "aws_security_group" "ssh" {
         protocol         = "tcp"
         cidr_blocks      = ["0.0.0.0/0"]
     }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-ssh"
+    }
+}
+
+resource "aws_security_group" "eb" {
+    name        = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-eb"
+    description = "Allow http/https inbound traffic"
+    vpc_id      = aws_vpc.vpc.id
 
     ingress {
         description      = "Allow http"
@@ -36,6 +54,58 @@ resource "aws_security_group" "ssh" {
     }
 
     tags = {
-        Name = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-ssh"
+        Name = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-eb"
+    }
+}
+
+resource "aws_security_group" "elasticache" {
+    name        = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-elasticache"
+    description = "Allow redis inbound traffic"
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        description      = "Allow redis"
+        from_port        = 6379
+        to_port          = 6379
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-elasticache"
+    }
+}
+
+resource "aws_security_group" "postgres" {
+    name        = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-postgres"
+    description = "Allow postgres inbound traffic"
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        description      = "Allow postgres"
+        from_port        = 5432
+        to_port          = 5432
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "${var.ORGANIZATION_NAME}-${var.SECURITY_GROUP_NAME}-postgres"
     }
 }

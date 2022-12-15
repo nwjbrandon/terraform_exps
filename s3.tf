07e772dcfc43c4d7087876fa4fe6bf49399ee504
nwjbrandon <nwjbrandon@gmail.com>
2022-12-15 16:39:25 +0800

    done

--- s3.tf
+++ s3.tf
@@ -273,3 +273,64 @@ data "aws_iam_policy_document" "pdf_policy_document" {
     ]
   }
 }
+
+###################
+#                 #
+# Bucket: pdf-dev #
+#                 #
+###################
+resource "aws_s3_bucket" "pdf-dev" {
+  bucket = "${var.ORGANIZATION_NAMESPACE}-pdf-dev"
+}
+
+resource "aws_s3_bucket_acl" "pdf-dev_acl" {
+  bucket = aws_s3_bucket.pdf-dev.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_cors_configuration" "pdf-dev_cors_configuration" {
+  bucket = aws_s3_bucket.pdf-dev.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD", "PUT"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket_policy" "pdf-dev_get_objects_policy" {
+  bucket = aws_s3_bucket.pdf-dev.id
+  policy = data.aws_iam_policy_document.pdf-dev_policy_document.json
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "pdf-dev_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.pdf-dev.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "pdf-dev_policy_document" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:DeleteBucket",
+    ]
+
+    effect = "Deny"
+
+    resources = [
+      aws_s3_bucket.pdf-dev.arn,
+      "${aws_s3_bucket.pdf-dev.arn}/*",
+    ]
+  }
+}

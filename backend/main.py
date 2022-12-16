e66f8958370876d11476355c9193fc8914b4b965
nwjbrandon <nwjbrandon@gmail.com>
2022-12-16 14:10:30 +0800

    python3 code

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description="Create Dockerrun.aws.json")
+parser.add_argument("--image_uri", required=True, type=str, help="docker image uri")
+
+args = parser.parse_args()
+image_uri = args.image_uri
+
+with open("Dockerrun.aws.template.json") as f:
+    config = json.load(f)
+
+config["Image"]["Name"] = image_uri
+
+with open("Dockerrun.aws.json", "f") as f:
+    json.dump(config, f, indent=2)
+
